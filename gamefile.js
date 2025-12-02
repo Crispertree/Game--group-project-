e101291642de478eff3d1ea9ce9d22daafde4d9c
Emmilampi <120257978+Emmilampi@users.noreply.github.com>
2025-12-02 09:01:45 +0100

    Remove test things, Add planned features

--- gamefile.js
+++ gamefile.js
@@ -1,4 +1,11 @@
-hej testar
-
-
-andra testgrej
+/*  
+  DOODLE JUMP GAME  
+  ------------------------------------
+  Features:
+  - Player (ball) auto-jumps upward
+  - Platforms (normal, moving, breaking)
+  - Scrolling upward simulation
+  - Start, Play, End screens
+  - OOP structure using classes
+  - Arrays + loops
+*/
