d5ed6f843da04bab135eac5bb294ce20580b5e05
Emmilampi <120257978+Emmilampi@users.noreply.github.com>
2025-12-09 10:19:24 +0100

    add basic start

--- gamefile.js
+++ gamefile.js
@@ -9,3 +9,46 @@
   - OOP structure using classes
   - Arrays + loops
 */
+let gameState = "start";
+let score = 0;
+
+let player;
+let platforms = []; /*array som håller alla platformar*/
+let numPlatforms = 10;
+
+
+
+class player {
+
+update (){ /*logik för rörelse*/}
+draw (){/*rita player*/}
+}
+
+
+class platform {
+
+}
+
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background();
+
+  if (gameState === "start") drawStartScreen();
+  else if (gameState === "playing") runGame();
+  else if (gameState === "gameover") drawGameOverScreen();
+
+}
+
+function drawGameOverScreen {
+
+
+
+    
+}
+
+function runGame {}
+
+function drawGameOverScreen {}
