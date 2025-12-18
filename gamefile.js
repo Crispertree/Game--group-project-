5a548a40ec3a1d17529041821d90b19bd0cb58e8
Emmilampi <120257978+Emmilampi@users.noreply.github.com>
2025-12-18 14:34:26 +0100

    g

--- gamefile.js
+++ gamefile.js
@@ -1,33 +1,53 @@
-/*  
-  DOODLE JUMP GAME  
-  ------------------------------------
-  Features:
-  - Player (ball) auto-jumps upward
-  - Platforms (normal, moving, breaking)
-  - Scrolling upward simulation
-  - Start, Play, End screens
-  - OOP structure using classes
-  - Arrays + loops
-*/
 let gameState = "start";
 let score = 0;
 
 let player;
-let platforms = []; /*array som håller alla platformar*/
-let numPlatforms = 10;
+let platforms = []; //array som håller alla platformar/
+let numPlatforms = 10; 
 
 
-
-class player {
-
-update (){ /*logik för rörelse*/}
-draw (){/*rita player*/}
+function setup() {
+  createCanvas(400, 600);
 }
 
+function draw() {
+  background(240);
 
-class platform {
-
+  if (gameState === "start") {
+    textAlign(CENTER);
+    textSize(24);
+    text("START", width / 2, height / 2);
+  }
 }
+
+class Player {
+  constructor() {
+    this.x = width / 2;
+    this.y = 100;
+    this.r = 10;
+    this.ySpeed = 0;
+    this.gravity = 0.4;
+  }
+}
+  update() {
+    this.ySpeed += this.gravity;
+    this.y += this.ySpeed;
+  }
+
+  show() {
+    ellipse(this.x, this.y, this.r * 2);
+  }
+
+
+class Platform {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 60;
+    this.h = 5;
+  }
+}
+
 
 function setup() {
   createCanvas(400, 600);
@@ -42,13 +62,20 @@ function draw() {
 
 }
 
-function drawGameOverScreen {
+
+function drawGameOverScreen () {
 
 
 
     
 }
 
-function runGame {}
+function runGame {
+
+
+
+/*koden här/*
+
+}
 
 function drawGameOverScreen {}
